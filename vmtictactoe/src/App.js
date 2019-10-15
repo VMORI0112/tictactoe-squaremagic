e235f8ba2e4c3bc9c9a8ab155b9f77aedd1e95ce
VMORI0112 <victorfernan_moriganador@hotmail.com>
2019-10-15 17:49:53 +0000

    tictacttoe commit2

---
+++ vmtictactoe/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Tictactoe from './components/tictactoe';
+
+function App() {
+  return (
+    <Tictactoe/>
+  );
+}
+
+export default App;
---
+++ vmtictactoe/src/components/tictactoe.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+
+import './squaremagic.css';
+
+const Tictactoe = () => {
+
+    //     const [flag, setFlag] = useState(0);
+    //     const [time, setTime] = useState(0);
+    //    // console.log("flag" + flag, "lights" + lightsoff)
+
+
+            const getplay = (event) => {
+            let idval = event.target.id;
+                  console.log(event.target);
+                // document.getElementById("yellowone").style.opacity = ".3";
+                // document.getElementById("greenone").style.opacity = ".3";
+                // document.getElementById("walk").style.opacity = ".3";
+                // document.getElementById("stop").style.opacity = ".3";
+            }
+
+
+    // useEffect(() => {
+
+    //     if (flag === 1 && lightsoff === 0)  {
+    //         setLightsoff(0);
+    //         setTimeout(() => setTime(time + 1), 1000);
+    //         if (time === 2)  onYellow();
+    //         if (time === 4)  onRed();
+    //         if (time === 10)  onGreen();
+    //         if (time === 16)  setTime(0);
+    //     }
+
+
+    // }, [time, flag, lightsoff])
+
+
+	return (
+       <>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-sm-2 ads"> "ads </div>
+            <div class="col-12 col-sm-6 content">
+                <div class="container-fluid tic-container">
+                <div class="row tic-row">
+                    <div class="col-4 tic-box" onClick={getplay} id="4"> 1 </div>
+                    <div class="col-4 tic-box" onClick={getplay} id="9"> 2 </div>
+                    <div class="col-4 tic-box" onClick={getplay} id="2"> 3 </div>
+                </div>
+                <div class="row tic-row">
+                    <div class="col-4 tic-box" onClick={getplay} id="3">4</div>
+                    <div class="col-4 tic-box" onClick={getplay} id="5">5 </div>
+                    <div class="col-4 tic-box" onClick={getplay} id="7">6 </div>
+                </div>
+                <div class="row tic-row">
+                    <div class="col-4 tic-box" onClick={getplay} id="8"> 7</div>
+                    <div class="col-4 tic-box" onClick={getplay} id="1"> 8</div>
+                    <div class="col-4 tic-box" onClick={getplay} id="6"> 9</div>
+                </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-2 tic-panel"> PANEL-THINGY<br/>Score:<br/>Foo bar: stuff<br/> Reset game and all that blank </div>
+             <div class="col-12 col-sm-2 ads"> ads </div>
+         </div>
+  </div>
+      </>
+	);
+
+};
+
+export default Tictactoe;
